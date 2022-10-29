47ebf432559384cd28d9190f079abe7de0dda96e
Coderofbrain <98619408+Coderofbrain@users.noreply.github.com>
2022-10-29 17:40:39 +0300

    Commit

---
+++ js/main.js
@@ -0,0 +1,66 @@
+
+const author = () = {
+
+return {avatar: `img/avatars/user{{xx}}.png`}
+};
+const offer = () => {
+  return{
+    title:
+
+    address: ,
+
+    price: ,
+
+    type: ,
+
+    rooms: ,
+
+    guests: ,
+
+    checkin: ,
+
+    checkout: ,
+
+    features = [wifi, dishwasher, parking, washer, elevator, conditioner]
+
+    description: ,
+
+    photos:}
+
+}
+
+
+// author =
+
+// {avatar};
+//строка — адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} — это число от 1 до 10. Перед однозначными числами ставится 0. Например, 01, 02...10. Адреса изображений не повторяются.
+// let offer = {
+// title, строка — заголовок предложения. Придумайте самостоятельно.
+
+// address, строка — адрес предложения. Для простоты пусть пока составляется из географических координат по маске {{location.lat}}, {{location.lng}}.
+
+// price, число — стоимость. Случайное целое положительное число.
+
+// type, строка — одно из пяти фиксированных значений: palace, flat, house, bungalow или hotel.
+
+// rooms, число — количество комнат. Случайное целое положительное число.
+
+// guests, число — количество гостей, которое можно разместить. Случайное целое положительное число.
+
+// checkin, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
+
+// checkout, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
+
+// features, массив строк — массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator, conditioner. Значения не должны повторяться.
+
+// description, строка — описание помещения. Придумайте самостоятельно.
+
+// photos, массив строк — массив случайной длины из значений: https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg.
+
+let location =
+
+{lat, //число с плавающей точкой — широта, случайное значение от 35.65000 до 35.70000.
+
+lng, //число с плавающей точкой — долгота, случайное значение от 139.70000 до 139.80000.}
+
+}
