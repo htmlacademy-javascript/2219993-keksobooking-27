448d4fb0d48aac53104f18cf8c876a82258c8231
Coderofbrain <98619408+Coderofbrain@users.noreply.github.com>
2022-11-16 00:15:19 +0300

    More corrections

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-// Генерация целого числа
-function getRandomInt(max, min) {
-  if (max < 0 || min < 0) {
-    console.log("NaN");
-  } else {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    console.log(Math.floor(Math.random() * (max - min + 1) + min));
-  }
-}
-
-//Генерация числа с плавающей точкой
-function getRandomFloat(min, max, precision) {
-  let x = Math.random() * (max - min + 1) + min;
-  if (x > max) {
-    x = Math.floor(x);
-    return x;
-  }
-  console.log(x.toFixed(precision));
-}
 const roomType = ["palace", "flat", "house", "bungalow", "hotel"];
 const checkIn = ["12:00", "13:00", "14:00"];
 const checkOut = ["12:00", "13:00", "14:00"];
